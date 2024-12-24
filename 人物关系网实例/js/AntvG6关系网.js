83edeabf49a0e3b090877970d83b50e7cad9bb54
WoHaoKun21 <609914130@qq.com>
2024-12-24 15:06:29 +0800

    change: 添加关系网页面文件

--- 人物关系网实例/js/AntvG6关系网.js
+++ 人物关系网实例/js/AntvG6关系网.js
@@ -1,51 +1,127 @@
-const getNodeSide = (graph, datum) => {
-  const parentData = graph.getParentData(datum.id, "tree");
-  if (!parentData) return "center";
-  return datum.style.x > parentData.style.x ? "right" : "left";
+const { ExtensionCategory, Graph, HoverActivate, idOf, register } = G6;
+const ACTIVE_COLOR = "#f6c523";
+const COLOR_MAP = {
+  "pre-inspection": "#3fc1c9",
+  problem: "#8983f3",
+  inspection: "#f48db4",
+  solution: "#ffaa64",
+};
+
+const getNode = ({ data }) => {
+  const { text, type } = data.data;
+  const isHovered = data.states?.includes("active");
+  const isSelected = data.states?.includes("selected");
+  const color = isHovered ? ACTIVE_COLOR : COLOR_MAP[type];
+  const containerStyle = `
+    width="100%";
+    height="100%";
+    background=${color};
+    borde= 3px solid ${color};
+    border-radius=16;
+    cursor=pointer;
+  `;
+
+  if (isSelected) {
+    Object.assign(containerStyle, { border: `3px solid #000` });
+  }
+  return `<div style=${containerStyle}>${text}</div>`;
 };
 
 const initialState = () => {
-  const { Graph, treeToGraphData } = G6;
-
   fetch("./json/AntvG6关系网.json")
-    .then((o) => o.json())
+    .then((d) => d.json())
     .then((data) => {
       const graph = new Graph({
         container: "container",
+        data,
         autoFit: "view",
-        data: treeToGraphData(data),
         node: {
-          style: {
-            labelText: (d) => d.id,
-            labelBackground: true,
-            labelPlacement: function (d) {
-              const side = getNodeSide(this, d);
-              return side === "center" ? "right" : side;
-            },
-            ports: [{ placement: "right" }, { placement: "left" }],
+          style: (d) => {
+            const style = {
+              component: getNode({ data: d }),
+              ports: [{ placement: "top" }, { placement: "bottom" }],
+            };
+            const size = {
+              "pre-inspection": [240, 120],
+              problem: [200, 120],
+              inspection: [330, 100],
+              solution: [200, 120],
+            }[d.data.type] || [200, 80];
+            Object.assign(style, {
+              size,
+              dx: -size[0] / 2,
+              dy: -size[1] / 2,
+            });
+            return style;
           },
-          animation: {
-            enter: false,
+          state: {
+            active: {
+              halo: false, // 光晕
+            },
+            selected: {
+              halo: false, // 选择后的光晕效果
+            },
           },
         },
         edge: {
-          type: "cubic-horizontal",
-          animation: {
-            enter: false,
+          type: "polyline",
+          style: {
+            lineWidth: 3,
+            radius: 20,
+            stroke: "#8b9baf",
+            endArrow: true,
+            labelText: (d) => d.data.text,
+            labelFill: "#8b9baf",
+            labelFontWeight: 600,
+            labelBackground: true,
+            labelBackgroundFill: "#f8f8f8",
+            labelBackgroundOpacity: 1,
+            labelBackgroundLineWidth: 3,
+            labelBackgroundStroke: "#8b9baf",
+            labelPadding: [1, 10],
+            labelBackgroundRadius: 4,
+            router: { type: "orth" },
+          },
+          state: {
+            active: {
+              stroke: ACTIVE_COLOR,
+              labelBackgroundStroke: ACTIVE_COLOR,
+              halo: false,
+            },
           },
         },
-        layout: {
-          type: "mindmap",
-          direction: "H",
-          getHeight: () => 32,
-          getWidth: () => 32,
-          getVGap: () => 4,
-          getHGap: () => 64,
-        },
-        behaviors: ["collapse-expand", "drag-canvas", "zoom-canvas"],
+        // // 节点悬浮窗
+        // plugins: [
+        //   {
+        //     type: "tooltip",
+        //     key: "tooltip",
+        //     enable: (event) =>
+        //       //启用node和canvas事件
+        //       event.targetType === "node" || event.targetType === "canvas",
+        //   },
+        // ],
+        layout: { type: "antv-dagre" },
+        behaviors: [
+          // "zoom-canvas", // 缩放
+          // "drag-canvas", // 拖拽
+          "hover-element", // 元素划入
+          "click-select", // 节点选择
+        ],
       });
-
       graph.render();
+      graph.on("node:click", (ev) => {
+        const { target, originalTarget } = ev;
+        // console.log("节点：", target.id, data);
+        console.log("node节点：", target.id, graph.getData());
+        // graph.hideElement(target.id); // 隐藏节点
+        // graph.showElement(target.id); // 展示节点
+      });
+      graph.on("edge:click", (ev) => {
+        const { target, originalTarget } = ev;
+        console.log("edge节点：", target.id);
+        // graph.hideElement(target.id); // 隐藏节点
+        // graph.showElement(target.id); // 展示节点
+      });
     });
 };
 
