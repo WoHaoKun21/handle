cc512c96cea07de96f8c586dcbf3cf2f1b871ee2
WoHaoKun21 <609914130@qq.com>
2025-04-02 14:00:43 +0800

    change:处理一些多余文件，添加高德气象文件

--- 高德地图/js/高德地图点聚合索引.js
+++ 高德地图/js/高德地图点聚合索引.js
@@ -1,9 +1,12 @@
-let satelliteLayer; // 卫星
-const count = points.length;
+const btn = document.getElementById("btn");
+let satelliteLayer; // 卫星底图
+let cluster; // 聚合标注对象
+let stations; // 水库列表
+let sum; // 数据总条数
 
 const initMap = () => {
   map = new AMap.Map("container", {
-    center: ["116.502159141883", "39.857389101662"],
+    center: [119.820345878417969, 30.0343912775878906],
     zooms: [5, 18], // 缩放范围
     animateEnable: true,
   });
@@ -14,109 +17,89 @@ const initMap = () => {
 
 initMap();
 
-// 可聚合的属性及范围
-const clusterIndexSet = {
-  city: {
-    minZoom: 2,
-    maxZoom: 10,
-  },
-  district: {
-    minZoom: 10,
-    maxZoom: 12,
-  },
-  area: {
-    minZoom: 12,
-    maxZoom: 15,
-  },
-  community: {
-    minZoom: 15,
-    maxZoom: 22,
-  },
-};
-
-// 聚合后的样式
-const getStyle = (context) => {
-  const clusterData = context.clusterData; // 聚合中包含数据
-  const index = context.index; // 聚合的条件
-  const count = context.count; // 聚合中点的总数
-  const marker = context.marker; // 聚合绘制点 Marker 对象
-  const color = ["8,60,156", "66,130,198", "107,174,214", "78,200,211"];
-  const indexs = ["city", "district", "area", "community"];
-  const i = indexs.indexOf(index["mainKey"]);
-  let text = clusterData[0][index["mainKey"]];
-  let size = Math.round(30 + Math.pow(count / points.length, 1 / 5) * 70);
-  if (i <= 2) {
-    const extra = '<span class="showCount">' + context.count + "套</span>";
-    text = '<span class="showName">' + text + "</span>";
-    text += extra;
-  } else {
-    size = 12 * text.length + 20;
-  }
-  const style = {
-    bgColor: "rgba(" + color[i] + ",.8)",
-    borderColor: "rgba(" + color[i] + ",1)",
-    text: text,
-    size: size,
-    index: i,
-    color: "#ffffff",
-    textAlign: "center",
-    boxShadow: "0px 0px 5px rgba(0,0,0,0.8)",
-  };
-  return style;
-};
-
-// 展开后标注的位置
-const getPosition = (context) => { 
-  const key = context.index.mainKey;
-  const dataItem = context.clusterData && context.clusterData[0];
-  const districtName = dataItem[key];
-  if (!district[districtName]) {
-    return null;
-  }
-  const center = district[districtName].center.split(",");
-  const centerLnglat = new AMap.LngLat(center[0], center[1]);
-  return centerLnglat;
-};
-
-// 自定义聚合点样式
+// 聚合后样式
 const _renderClusterMarker = (context) => {
-  const clusterData = context.clusterData; // 聚合中包含数据
-  const index = context.index; // 聚合的条件
-  const count = context.count; // 聚合中点的总数
-  const marker = context.marker; // 聚合点标记对象
-  const styleObj = getStyle(context);
-  // 自定义点标记样式
+  const { marker, count, clusterData } = context;
   const div = document.createElement("div");
-  div.className = "amap-cluster";
-  div.style.backgroundColor = styleObj.bgColor;
-  div.style.width = styleObj.size + "px";
-  if (styleObj.index <= 2) {
-    div.style.height = styleObj.size + "px";
+  if (count >= 0 && count < 10) {
+    div.classList.add("ten");
+  } else if (count >= 10 && count < 30) {
+    div.classList.add("thirty");
+  } else if (count >= 30 && count < 60) {
+    div.classList.add("sixty");
+  } else if (count >= 60 && count < 100) {
+    div.classList.add("oneHundred");
+  } else if (count >= 100) {
+    div.classList.add("max");
+  }
+
+  if (count > 1) {
     // 自定义点击事件
-    context.marker.on("click", function (e) {
-      let curZoom = map.getZoom();
-      if (curZoom < 20) {
-        curZoom += 1;
-      }
-      map.setZoomAndCenter(curZoom, e.lnglat);
+    marker.on("click", () => {
+      const markerList = clusterData.map(
+        (o) =>
+          new AMap.Marker({
+            position: o.lnglat,
+          })
+      );
+      map.setFitView(markerList, false);
     });
   }
-  div.style.border = "solid 1px " + styleObj.borderColor;
-  div.style.borderRadius = styleObj.size + "px";
-  div.innerHTML = styleObj.text;
-  div.style.color = styleObj.color;
-  div.style.textAlign = styleObj.textAlign;
-  div.style.boxShadow = styleObj.boxShadow;
-  context.marker.setContent(div);
-  // 自定义聚合点标记显示位置
-  const position = getPosition(context);
-  if (position) {
-    context.marker.setPosition(position);
-  }
-  context.marker.setAnchor("center");
+  const size = Math.round(25 + Math.pow(count / sum, 1 / 5) * 40);
+  div.innerHTML = count;
+  marker.setOffset(new AMap.Pixel(-size / 2, -size / 2));
+  marker.setContent(div);
 };
 
-const cluster = new AMap.IndexCluster(map, points, {
-  renderClusterMarker: _renderClusterMarker,
-  clusterIndexSet: clusterIndexSet,
-});
+// 展开后样式
+const _renderMarker = (context) => {
+  const content = `
+      <div class="cstmIconPanel">
+        <div class="cstmIconName">
+          <div>
+            <img src="./img/dwImg.png" alt="" />
+          </div>
+          <p>测试水库</p>
+        </div>
+        <div class="cstmIconImg">
+          <img src='./img/dwImg.png' style="width: 60px" />
+        </div>
+      </div>
+    `;
+  const offset = new AMap.Pixel(-22, -37);
+  context.marker.setContent(content);
+  context.marker.setOffset(offset);
+};
+
+// 获取水库列表
+const getReserList = async () => {
+  const res = await fetch("./json/高德地图点聚合 copy.json");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const jsonData = await res.json();
+  stations = jsonData
+    .filter((o) => o.coord)
+    .map((o) => ({ ...o, lnglat: o.coord.split(",").map(Number) }));
+  sum = stations.length;
+
+  // 聚合样式类
+  cluster = new AMap.MarkerCluster(map, stations, {
+    gridSize: 120, // 聚合网格像素大小
+    renderClusterMarker: _renderClusterMarker, // 自定义聚合点样式
+    renderMarker: _renderMarker, // 自定义非聚合点样式
+  });
+};
+
+getReserList();
+
+btn.onclick = () => {
+  console.log("标注：", cluster);
+  cluster.setMap(null); // 将聚合标注从地图上移除
+  // 聚合样式类
+  cluster = new AMap.MarkerCluster(map, stations, {
+    gridSize: 120, // 聚合网格像素大小
+    renderClusterMarker: _renderClusterMarker, // 自定义聚合点样式
+    renderMarker: _renderMarker, // 自定义非聚合点样式
+  });
+};
